eaed7920ca1f8d16e33b155581e698ff17363275
Seu Nome <pedro.live.martins@gmail.com>
2025-05-14 01:02:07 -0300

    Add babel.config

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+// App.tsx
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-import 'react-native-gesture-handler';
+
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
